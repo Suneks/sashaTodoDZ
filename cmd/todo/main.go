ec98d33bd7742bca30e2cf34e44b8b990f2ae77a
KhorkinAN <aleksandr.khorkin@vitech.team>
2025-10-02 15:30:35 +0300

    добавил мьютексы в ветку AddMutex

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,84 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"sync"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	apiTasks "toDo/internal/service/api/tasks"
+	coreTasks "toDo/internal/service/core/tasks"
 )
 
-//func main() {
-//	// Создаем хранилище
-//	storage := coreTasks.NewInMemoryStorage()
-//
-//	// Создаем хендлер
-//	taskHandler := apiTasks.NewTaskHandler(storage)
-//
-//	// Создаем роутер
-//	r := mux.NewRouter()
-//
-//	// Настраиваем маршруты
-//	r.HandleFunc("/tasks", taskHandler.CreateTask).Methods("POST")
-//	r.HandleFunc("/tasks", taskHandler.GetAllTasks).Methods("GET")
-//	r.HandleFunc("/tasks/{id}", taskHandler.GetTaskByID).Methods("GET")
-//	r.HandleFunc("/tasks/{id}", taskHandler.UpdateTask).Methods("PUT")
-//	r.HandleFunc("/tasks/{id}", taskHandler.DeleteTask).Methods("DELETE")
-//
-//	// Запускаем сервер
-//	log.Println("Server starting on :8080")
-//	log.Fatal(http.ListenAndServe(":8080", r))
-//}
-
-// пример 1
 func main() {
-	var wg sync.WaitGroup
-	c := make(chan string, 5)
+	// Создаем хранилище
+	storage := coreTasks.NewInMemoryStorage()
 
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func(с chan<- string, i int, group *sync.WaitGroup) {
-			defer wg.Done()
-			с <- fmt.Sprintf("Goroutine %s", strconv.Itoa(i))
-		}(c, i, &wg)
-	}
+	// Создаем хендлер
+	taskHandler := apiTasks.NewTaskHandler(storage)
 
-	go func() {
-		wg.Wait()
-		close(c)
-	}()
+	// Создаем роутер
+	r := mux.NewRouter()
 
-	for {
-		select {
-		case v := <-c:
-			fmt.Println(v)
-		}
-	}
+	// Настраиваем маршруты
+	r.HandleFunc("/tasks", taskHandler.CreateTask).Methods("POST")
+	r.HandleFunc("/tasks", taskHandler.GetAllTasks).Methods("GET")
+	r.HandleFunc("/tasks/{id}", taskHandler.GetTaskByID).Methods("GET")
+	r.HandleFunc("/tasks/{id}", taskHandler.UpdateTask).Methods("PUT")
+	r.HandleFunc("/tasks/{id}", taskHandler.DeleteTask).Methods("DELETE")
+
+	// Запускаем сервер
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
-
-// пример 2. Вывод 1 0
-//func main() {
-//	a := 0
-//	defer fmt.Println(a)
-//	defer func() {
-//		a++
-//		fmt.Println(a)
-//	}()
-//}
-
-// пример 3. Вывод hello world panic
-//func main() {
-//	defer fmt.Println("world")
-//	fmt.Println("hello")
-//	panic("error")
-//}
-
-// пример 4. Изначально паника из-за того что записываем в nil мапу, Инициализировал через make
-//func main() {
-//	var m map[string]int
-//	m = make(map[string]int)
-//
-//	fmt.Println(m["foo"])
-//
-//	m["foo"] = 42
-//
-//	fmt.Println(m["foo"])
-//}
